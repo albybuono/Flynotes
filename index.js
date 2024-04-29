e4fa541a4baac771222a6324d9f7141c1f252d6b
albybuono <145298304+albybuono@users.noreply.github.com>
2024-04-29 15:57:10 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,40 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var hamburger = document.querySelector('.hamburger');
-    var menu = document.querySelector('.menu');
+$(document).ready(function(){
+    // Funzione per caricare i messaggi nella chat
+    function loadChat() {
+        $.ajax({
+            url: 'load_chat.php',
+            type: 'GET',
+            success: function(response) {
+                $('#chat-box').html(response);
+            }
+        });
+    }
 
-    hamburger.addEventListener('touchstart', function() {
-    
-        menu.classList.toggle('menu--open');
+    // Carica i messaggi nella chat all'avvio della pagina
+    loadChat();
+
+    // Invia un messaggio quando l'utente preme il pulsante "Invia"
+    $('#message-form').submit(function(event) {
+        event.preventDefault(); // Evita il comportamento predefinito del form
+
+        var formData = new FormData($(this)[0]);
+
+        $.ajax({
+            url: 'send_message.php',
+            type: 'POST',
+            data: formData,
+            contentType: false,
+            processData: false,
+            success: function(response) {
+                loadChat(); // Aggiorna la chat dopo l'invio del messaggio
+                $('#message-input').val(''); // "Pulisce" l'input del messaggio dopo l'invio
+            },
+            error: function(xhr, status, error) {
+                console.error("Errore durante l'invio del messaggio:", error);
+            }
+        });
     });
-});
 
-document.addEventListener('DOMContentLoaded', function () {
-    var header = document.querySelector('.header');
-    var hero = document.querySelector('.hero');
-    var content = document.querySelector('.content');
-
-    window.addEventListener('scroll', function () {
-        var heroBottom = hero.offsetTop + hero.offsetHeight;
-        if (window.pageYOffset >= heroBottom) {
-            header.classList.add('header-dark');
-        } else {
-            header.classList.remove('header-dark');
-        }
-    });
+    // Funzione per aggiornare la chat ogni 2 secondi
+    setInterval(loadChat, 2000); // 2000 millisecondi = 2 secondi
 });
